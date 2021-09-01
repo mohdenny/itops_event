1d396e95eb7abad20dcab9686ee3864e6aa01a95
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-02 05:02:38 +0700

    delete props

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
     return (
         <Fragment>
             <div className="max-w-7xl mx-auto py-6 px-6 h-screen">
-                <MyCalendar onlyview={true} />
+                <MyCalendar />
             </div>
         </Fragment>
     )
