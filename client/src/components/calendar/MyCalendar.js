fa45227408152be00dc83bb2e55b4761c3edb55c
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-13 01:00:04 +0700

    filter event and change high

--- client/src/components/calendar/MyCalendar.js
+++ client/src/components/calendar/MyCalendar.js
@@ -1,57 +1,27 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { getEvents, getEventsGuest } from '../../actions/event';
+import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const MyCalendar = ({ 
-  event: { events }, 
-  auth: { isAuthenticated }, 
-  getEventsGuest, 
-  getEvents 
-}) => {
+const MyCalendar = ({ events }) => {
     const localizer = momentLocalizer(moment);
 
-    useEffect(() => {
-      if(!isAuthenticated) {
-        getEventsGuest();
-      }
-
-      getEvents();
-
-    }, [isAuthenticated, getEventsGuest, getEvents])
-
     return (
-        <div className='max-w-full bg-gray-300 rounded-xl mb-10 h-full' >
+        <div className='max-w-full bg-gray-300 rounded-xl overflow-auto'>
           <div className="px-6 py-4 text-center">
             <div className="font-bold text-lg mb-2">Calendar</div>
           </div>
-              {/* <Calendar
-                events={events.filter(event => event.status === 'upcoming')}
+              <Calendar
+                events={events.filter(event => event.status === 'upcoming' || event.status === 'ongoing')}
                 startAccessor={(events) => { return moment(events.start).toDate() }}
                 endAccessor={(events) => { return moment(events.end).toDate() }}
                 defaultDate={moment().toDate()}
                 localizer={localizer}
                 className="bg-gray-200 p-4"
-              /> */}
-              <div className="border bg-white h-full">ok
-              </div>
+                style={{ height: '500px' }} 
+              />
         </div>
     );
 }
 
-Calendar.defaultProps = {
-  getEvents: PropTypes.isNotNull,
-  getEventsGuest: PropTypes.isNotNull,
-  event: PropTypes.isNotNull,
-  auth: PropTypes.isNotNull 
-}
-
-const mapStateToProps = state => ({
-  event: state.event,
-  auth: state.auth
-})
-
-export default connect(mapStateToProps, { getEventsGuest, getEvents })(MyCalendar);
+export default MyCalendar;
