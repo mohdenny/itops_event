1836b09be313676e74fb6d6c48fa65cd17a8bd12
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-13 00:59:40 +0700

    conditional text size

--- client/src/components/card-event/CardEvent.js
+++ client/src/components/card-event/CardEvent.js
@@ -1,20 +1,29 @@
 import React, { Fragment } from 'react';
 import CardItem from './CardItem';
 
-const CardEvent = ({ events }) => {
+const CardEvent = ({ path, events, title, sortText, color, high, highBody=null }) => {
     const filteredEvent = (filter) => {
         return events.filter(event => event.status === filter)
     }
 
-    const renderedCard = (events, title, sortText, color) => {
+    const renderedItem = (pathname) => {
+        switch(`${pathname}`){
+            case '/':
+                return (<CardItem events={events} sortText={sortText} titleSize={'3xl'} textSize={'3xl'} />)
+            default:
+                return (<CardItem events={events} sortText={sortText} titleSize={'lg'} textSize={'base'} />)
+        }
+    }
+
+    const renderedCard = () => {
         return (
-            <div className={`max-w-full overflow-hidden bg-${color}-200 rounded-xl h-full`}>
+            <div className={`max-w-full overflow-hidden bg-${color}-200 rounded-xl text ${high}`}>
                 <div className="px-6 py-4 text-center">
                     <div className="font-bold text-lg mb-2">{title}</div>
                 </div>
                 { filteredEvent(sortText).length > 0 ? (
-                    <div className={`bg-${color}-100 px-4 py-4 overflow-auto max-h-64 h-auto`}>
-                        <CardItem events={events} sortText={sortText} />
+                    <div className={`bg-${color}-100 px-4 py-4 overflow-auto ${highBody} ${high}`}>
+                        {renderedItem(path)}
                     </div>
                 ) : (
                     <div className={`bg-${color}-100 px-4 py-4 h-full`}>
@@ -27,10 +36,7 @@ const CardEvent = ({ events }) => {
 
     return (
         <Fragment>
-            <div className="grid grid-cols-2 gap-4 mb-4">
-                {renderedCard(events, 'New','', 'yellow')}
-                {renderedCard(events, 'Upcoming','upcoming', 'green')}
-            </div>
+            {renderedCard()}
         </Fragment>
     );
 };
