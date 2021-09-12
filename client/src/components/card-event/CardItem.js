02a86116e7af65636eba06926f1494789fbcbfd9
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-13 00:59:18 +0700

    add props

--- client/src/components/card-event/CardItem.js
+++ client/src/components/card-event/CardItem.js
@@ -2,18 +2,21 @@ import React, { Fragment } from 'react';
 import { Link }  from 'react-router-dom';
 import moment from 'moment';
 
-const CardItem = ({events, sortText }) => {
+const CardItem = ({events, sortText, titleSize, textSize }) => {
     const renderedList = events
         .sort((a, b) => a.start.localeCompare(b.start))
             .filter(event => event.status === sortText)
                 .map(filteredEvent => {
                     return (
-                        <div key={filteredEvent._id} className="bg-white px-4 py-2 mb-2 rounded-xl shadow-md grid grid-cols-2 gap-4 h-full">
-                            <Link to={`/events/${filteredEvent._id}`} className="col-span-full"><p className="inline-block text-lg hover:text-gray-500">{filteredEvent.title}</p></Link>
-                            <p className="inline-block text-xs col-span-1">
-                                { `${moment(filteredEvent.start).format("lll")} - ${moment(filteredEvent.end).format("lll")}` }
-                            </p>
-                            <p className="inline-block text-xs col-span-1 text-right">{filteredEvent.location}</p>
+                        <div key={filteredEvent._id} className="bg-white p-4 mb-2 rounded-xl shadow-md grid grid-flow-row auto-rows-max md:auto-rows-min">
+                            <Link to={`/events/${filteredEvent._id}`} ><p className={`lg:text-${titleSize} md:text-md sm:text-sm font-bold hover:text-gray-500`}>{filteredEvent.title}</p></Link>
+                            <pre className={`lg:text-${textSize} md:text-sm sm:text-xs`}>
+                                Start: { `${moment(filteredEvent.start).format("D-MM-YYYY, H:mm")}` }
+                                <pre>
+                                End: { `${moment(filteredEvent.end).format("D-MM-YYYY, H:mm")}` }
+                                </pre>
+                            </pre>
+                            <p className={`lg:text-${textSize} md:text-sm sm:text-xs`}>{filteredEvent.location}</p>
                         </div>
                     );
                 });
