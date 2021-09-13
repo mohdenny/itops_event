f8dc32b300751eb1f1ef55777f6def9419306d5e
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-13 15:59:14 +0700

    overflow auto calendar

--- client/src/components/calendar/MyCalendar.js
+++ client/src/components/calendar/MyCalendar.js
@@ -7,19 +7,19 @@ const MyCalendar = ({ events }) => {
     const localizer = momentLocalizer(moment);
 
     return (
-        <div className='max-w-full bg-gray-300 rounded-xl overflow-auto'>
+        <div className='max-w-full bg-red-300 overflow-auto rounded-xl'>
           <div className="px-6 py-4 text-center">
             <div className="font-bold text-lg mb-2">Calendar</div>
           </div>
-              <Calendar
-                events={events.filter(event => event.status === 'upcoming' || event.status === 'ongoing')}
-                startAccessor={(events) => { return moment(events.start).toDate() }}
-                endAccessor={(events) => { return moment(events.end).toDate() }}
-                defaultDate={moment().toDate()}
-                localizer={localizer}
-                className="bg-gray-200 p-4"
-                style={{ height: '500px' }} 
-              />
+            <Calendar
+              events={events.filter(event => event.status === 'upcoming' || event.status === 'ongoing')}
+              startAccessor={(events) => { return moment(events.start).toDate() }}
+              endAccessor={(events) => { return moment(events.end).toDate() }}
+              defaultDate={moment().toDate()}
+              localizer={localizer}
+              className="bg-gray-200 p-4"
+              style={{ height: '600px' }} 
+            />
         </div>
     );
 }
