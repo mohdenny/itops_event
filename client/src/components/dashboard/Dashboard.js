9e0a1c10aa55f444b6489f1b7921ff883c71d4a4
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-14 07:11:10 +0700

    set col and row

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -27,14 +27,14 @@ const Dashboard = ({
                         Welcome { user && user.name }
                     </div>
                     <Alert />
-                    <div className="grid grid-flow-col grid-cols-2 grid-rows-auto gap-4 p-4 bg-white rounded-lg max-w-full">
-                        <div className="grid grid-flow-col lg:grid-cols-2 lg:grid-rows-2 md:grid-cols-1 md:grid-rows-4 gap-4 mb-4">
-                            <CardEvent events={events} title={'new'} sortText={''} color={'yellow'} high={'h-full'} highBody={'max-h-64'} />
-                            <CardEvent events={events} title={'ongoing'} sortText={'ongoing'} color={'blue'} high={'h-full'} highBody={'max-h-64'} />
-                            <CardEvent events={events} title={'upcoming'} sortText={'upcoming'} color={'green'} high={'h-full'} highBody={'max-h-64'} />
-                            <CardEvent events={events} title={'done'} sortText={'done'} color={'gray'} high={'h-full'} highBody={'max-h-64'} />
+                    <div className="grid lg:grid-cols-2 sm:grid-rows-auto gap-4 p-4 bg-white rounded-lg max-w-full">
+                        <div className="grid lg:grid-cols-2 sm:grid-rows-auto gap-4 mb-4">
+                            <CardEvent events={events} title={'new'} sortText={''} color={'yellow'} high={'h-full'} highBody={'max-h-auto'} />
+                            <CardEvent events={events} title={'upcoming'} sortText={'upcoming'} color={'green'} high={'h-full'} highBody={'max-h-auto'} />
+                            <CardEvent events={events} title={'ongoing'} sortText={'ongoing'} color={'blue'} high={'h-full'} highBody={'max-h-auto'} />
+                            <CardEvent events={events} title={'done'} sortText={'done'} color={'gray'} high={'h-full'} highBody={'max-h-auto'} />
                         </div>
-                        <div className="grid grid-flow-col lg:grid-cols-auto lg:grid-rows-1 gap-4">
+                        <div className="grid lg:grid-cols-1 sm:grid-rows-auto gap-4">
                             <MyCalendar events={events} />
                         </div>
                     </div>
