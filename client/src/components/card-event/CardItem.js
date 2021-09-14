64336a1c9131df11d1a9835e312de01ab8cedcbb
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-14 07:10:56 +0700

    set border bottom title

--- client/src/components/card-event/CardItem.js
+++ client/src/components/card-event/CardItem.js
@@ -9,7 +9,7 @@ const CardItem = ({events, sortText, titleSize, textSize }) => {
                 .map(filteredEvent => {
                     return (
                         <div key={filteredEvent._id} className="bg-white p-4 mb-2 rounded-xl shadow-md grid grid-flow-row auto-rows-max md:auto-rows-min">
-                            <Link to={`/events/${filteredEvent._id}`} ><p className={`lg:text-${titleSize} md:text-md sm:text-sm font-bold hover:text-gray-500`}>{filteredEvent.title}</p></Link>
+                            <Link to={`/events/${filteredEvent._id}`}><p className={`lg:text-${titleSize} md:text-md sm:text-sm font-bold hover:text-gray-500 border-b-2 border-gray-300`}>{filteredEvent.title}</p></Link>
                             <pre className={`lg:text-${textSize} md:text-sm sm:text-xs`}>
                                 Start: { `${moment(filteredEvent.start).format("D-MM-YYYY, H:mm")}` }
                                 <pre>
