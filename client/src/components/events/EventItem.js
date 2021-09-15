5547262761ae22da2d2e44857348e5ac952b862f
Mohammad Denny <mohammaddenny94@gmail.com>
2021-09-15 19:12:01 +0700

    update filter status

--- client/src/components/events/EventItem.js
+++ client/src/components/events/EventItem.js
@@ -10,6 +10,7 @@ import PdfDocument from '../pdf/PdfDocument';
 
 const EventItem = ({ events, updateStatus, deleteEvent }) => {
     const [status, setStatus] = useState();
+    const [text, setText] = useState('all');
 
     useEffect(() => {
         setStatus({status: 'done'});
@@ -50,66 +51,82 @@ const EventItem = ({ events, updateStatus, deleteEvent }) => {
                 </p> 
             )
         }
+    } 
+
+    const filterButton = filterText => {
+        setText(filterText)
+        console.log(text)
     }
 
-    const renderedList = events.map(event => {
-        return (
-            <tr key={event._id}>
-                <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="flex items-center">
-                        <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">
-                                <Link to={`/events/${event._id}`} className="text-lg hover:text-gray-500">
-                                    {event.title}
-                                </Link>
-                                
-                                {warningRequired(event, 'yellow', 'Items and Support are required!')}
-                                {warningStatus(event, 'upcoming', 'green', 'This event is Upcoming!')}
-                                {warningStatus(event, 'ongoing', 'blue', 'Cannot be edited during this event Ongoing!')}
-                                {warningStatus(event, 'done', 'gray', 'This event has ended!')}
+    const checkFilter = (event, text) => {
+        if(text === 'all') {
+            console.log(text)
+            return event.status === text
+        }else{
+            return event
+        }
+    }
+
+    const renderedList = events
+        .sort((a, b) => a.start.localeCompare(b.start))
+            .filter(event => checkFilter(event, text)).map(event => {
+                return (
+                    <tr key={event._id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                            <div className="flex items-center">
+                                <div className="ml-4">
+                                    <div className="text-sm font-medium text-gray-900">
+                                        <Link to={`/events/${event._id}`} className="text-lg hover:text-gray-500">
+                                            {event.title}
+                                        </Link>
+                                        
+                                        {warningRequired(event, 'yellow', 'Items and Support are required!')}
+                                        {warningStatus(event, 'upcoming', 'green', 'This event is Upcoming!')}
+                                        {warningStatus(event, 'ongoing', 'blue', 'Cannot be edited during this event Ongoing!')}
+                                        {warningStatus(event, 'done', 'gray', 'This event has ended!')}
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-900">{ `Start: ${formatDate(event.start)}` } </div>
-                    <div className="text-sm text-gray-900">{ `End: ${formatDate(event.end)}` } </div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {event.location}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                    <span className={`${setColorStatus(event.status)} px-2 inline-flex text-xs leading-5 font-semibold rounded-full`}>
-                        {event.status === '' ? 'new' : event.status}
-                    </span>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-900">{formatDate(event.date)} </div>
-                </td>
-                <td className="px-4 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    {
-                        event.status === 'ongoing' ?   
-                            (<button onClick={() => updateStatus(event._id, status)} type="button" className="ml-2 p-1 bg-gray-500 rounded-md text-white hover:bg-gray-800 hover:text-gray-200" >Done</button>) 
-                        :
-                            (<Link to={`/events/event/${event._id}`} className={`${event.status === 'done' ? 'hidden' : '' } ml-2 p-1 bg-indigo-500 rounded-md text-white hover:bg-indigo-800 hover:text-gray-200`}>Edit</Link>)
-                    }
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                            <div className="text-sm text-gray-900">{ `Start: ${formatDate(event.start)}` } </div>
+                            <div className="text-sm text-gray-900">{ `End: ${formatDate(event.end)}` } </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {event.location}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                            <span className={`${setColorStatus(event.status)} px-2 inline-flex text-xs leading-5 font-semibold rounded-full`}>
+                                {event.status === '' ? 'new' : event.status}
+                            </span>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                            <div className="text-sm text-gray-900">{formatDate(event.date)} </div>
+                        </td>
+                        <td className="px-4 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            {
+                                event.status === 'ongoing' ?   
+                                    (<button onClick={() => updateStatus(event._id, status)} type="button" className="ml-2 p-1 bg-gray-500 rounded-md text-white font-semibold hover:bg-gray-800 hover:text-gray-200" >Done</button>) 
+                                :
+                                    (<Link to={`/events/event/${event._id}`} className={`${event.status === 'done' ? 'hidden' : '' } ml-2 p-1 bg-indigo-500 rounded-md text-white hover:bg-indigo-800 hover:text-gray-200`}>Edit</Link>)
+                            }
 
-                    {
-                        event.status !== 'ongoing' && <button onClick={() => deleteEvent(event._id)} type="button" className="ml-2 p-1 bg-red-500 rounded-md text-white hover:bg-red-800 hover:text-gray-200">Delete</button>
-                    }
+                            {
+                                event.status !== 'ongoing' && event.status !== 'done' && <button onClick={() => deleteEvent(event._id)} type="button" className="ml-2 p-1 bg-red-500 rounded-md text-white font-semibold hover:bg-red-800 hover:text-gray-200">Delete</button>
+                            }
 
-                    <PDFDownloadLink 
-                        document={<PdfDocument event={event} />}
-                        fileName={`${event.title}.pdf`}
-                        className="ml-2 p-1 bg-yellow-500 rounded-md text-white hover:bg-yellow-800 hover:text-yellow-200"
-                    >
-                        PDF
-                    </PDFDownloadLink >
-                    
-                </td>
-            </tr>
-        )
-    })
+                            <PDFDownloadLink 
+                                document={<PdfDocument event={event} />}
+                                fileName={`${event.title}.pdf`}
+                                className="ml-2 p-1 bg-yellow-500 rounded-md text-white hover:bg-yellow-800 hover:text-yellow-200"
+                            >
+                                PDF
+                            </PDFDownloadLink >
+                            
+                        </td>
+                    </tr>
+                )
+            })
 
     return (
         <Fragment>
@@ -118,6 +135,20 @@ const EventItem = ({ events, updateStatus, deleteEvent }) => {
                     <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div className="shadow overflow-hidden border-b border-gray-200 bg-white sm:rounded-lg">
                         <Alert />
+                            <div className="grid grid-cols-4 gap-4 justify-items-center border mb-2">
+                                <div className="border">
+                                    <button onClick={() => filterButton('')} type="button">new</button>
+                                </div>
+                                <div className="border">
+                                    <button onClick={() => filterButton('upcoming')} type="button">upcoming</button>
+                                </div>
+                                <div className="border">
+                                    <button onClick={() => filterButton('ongoing')} type="button">ongoing</button>
+                                </div>
+                                <div className="border">
+                                    <button onClick={() => filterButton('done')} type="button">done</button>
+                                </div>
+                            </div>
                             <table className="min-w-full divide-y divide-gray-200">
                                 <thead className="bg-gray-50">
                                     <tr>
