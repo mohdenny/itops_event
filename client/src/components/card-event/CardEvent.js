9d3d9f0e99b4c51c9843a6c58b143eda45c04c03
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-18 18:26:16 +0700

    add whispace-normal

--- client/src/components/card-event/CardEvent.js
+++ client/src/components/card-event/CardEvent.js
@@ -9,7 +9,7 @@ const CardEvent = ({ path, events, title, sortText, color, high, highBody }) => 
     const renderedItem = (pathname) => {
         switch(`${pathname}`){
             case '/':
-                return (<CardItem events={events} sortText={sortText} titleSize={'text-3xl'} textSize={'text-3xl'} />)
+                return (<CardItem events={events} sortText={sortText} titleSize={'text-3xl'} textSize={'text-2xl'} />)
             default:
                 return (<CardItem events={events} sortText={sortText} titleSize={'text-lg'} textSize={'text-base'} />)
         }
--- client/src/components/card-event/CardItem.js
+++ client/src/components/card-event/CardItem.js
@@ -8,27 +8,31 @@ const CardItem = ({events, sortText, titleSize, textSize }) => {
             .filter(event => event.status === sortText)
                 .map(filteredEvent => {
                     return (
-                        <div key={filteredEvent._id} className="bg-white p-4 mb-2 rounded-xl shadow-md grid grid-flow-row auto-rows-max overflow-auto md:auto-rows-min ">
-                            <Link to={`/events/${filteredEvent._id}`}><p className={`lg:${titleSize} md:text-md sm:text-sm font-bold hover:text-gray-500`}>{filteredEvent.title.toUpperCase()}</p></Link>
-                            <p className={`lg:${textSize} md:text-sm sm:text-xs border-b-2 border-gray-300`} >
-                                {filteredEvent.description}
-                            </p>
+                        <div key={filteredEvent._id} className="bg-white p-4 mb-2 rounded-xl shadow-md grid grid-flow-row auto-rows-max md:auto-rows-min h-auto w-full">
+                            <div className="border-b-2 border-gray-300 w-full h-full ">
+                                <Link to={`/events/${filteredEvent._id}`}><p className={`lg:${titleSize} md:text-md sm:text-sm font-bold hover:text-gray-500`}>{filteredEvent.title.toUpperCase()}</p></Link>
+                                <p className={`lg:${textSize} md:text-sm sm:text-xs whitespace-normal`} >
+                                    {filteredEvent.description}
+                                </p>
+                            </div>
                             <pre className={`lg:${textSize} md:text-sm sm:text-xs`}>
                                 <pre>
-                                Start: { `${moment(filteredEvent.start).format("D-MM-YYYY, H:mm")}` }
+                                <b>Start:</b> { `${moment(filteredEvent.start).format("D-MM-YYYY, H:mm")}` }
                                 </pre>
                                 <pre>
-                                End: { `${moment(filteredEvent.end).format("D-MM-YYYY, H:mm")}` }
+                                <b>End:</b> { `${moment(filteredEvent.end).format("D-MM-YYYY, H:mm")}` }
                                 </pre>
-                                <pre>
+                                <pre className="whitespace-normal">
                                     {filteredEvent.location}
                                 </pre>
                             </pre>
-                            {filteredEvent.supports.map(support => {
-                                (<p className={`lg:${textSize} md:text-sm sm:text-xs`} key={support.id}>
-                                    {support.name_support}
-                                </p>)
-                            })}
+                            { filteredEvent.supports.map(support => {
+                                return (
+                                    <pre className={`lg:${textSize} md:text-sm sm:text-xs`} key={support.id}>
+                                        <b>Support:</b> {support.name_support}
+                                    </pre>
+                                )
+                            }) }
                         </div>
                     );
                 });
