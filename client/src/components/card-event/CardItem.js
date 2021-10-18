f9d6ef1f9bdf709fc4b9f0e3bee3e2806b05f9d3
Mohammad Denny <mohammaddenny94@gmail.com>
2021-10-18 17:55:13 +0700

    add toUpperCase

--- client/src/components/card-event/CardItem.js
+++ client/src/components/card-event/CardItem.js
@@ -9,7 +9,7 @@ const CardItem = ({events, sortText, titleSize, textSize }) => {
                 .map(filteredEvent => {
                     return (
                         <div key={filteredEvent._id} className="bg-white p-4 mb-2 rounded-xl shadow-md grid grid-flow-row auto-rows-max overflow-auto md:auto-rows-min ">
-                            <Link to={`/events/${filteredEvent._id}`}><p className={`lg:${titleSize} md:text-md sm:text-sm font-bold hover:text-gray-500`}>{filteredEvent.title}</p></Link>
+                            <Link to={`/events/${filteredEvent._id}`}><p className={`lg:${titleSize} md:text-md sm:text-sm font-bold hover:text-gray-500`}>{filteredEvent.title.toUpperCase()}</p></Link>
                             <p className={`lg:${textSize} md:text-sm sm:text-xs border-b-2 border-gray-300`} >
                                 {filteredEvent.description}
                             </p>
